af4d8c4fd07477c4961ffea010c514f2544edc01
Randy McKown <72272050+nerdvoodoo@users.noreply.github.com>
2024-11-04 09:22:33 -0600

    Initial Commit

---
+++ Sig/SigApp.swift
@@ -0,0 +1,17 @@
+//
+//  SigApp.swift
+//  Sig
+//
+//  Created by Randy McKown on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
